ca44ad706283468801c91d6c2e9a9e3cd1c5e8bb
Steve Calvert <scalvert@linkedin.com>
2017-11-26 15:40:14 -0800

    Pulling functions in from module scope

--- ember-sinon-sandbox-codemod.js
+++ ember-sinon-sandbox-codemod.js
@@ -1,35 +1,35 @@
-function isSandboxAssignment(j, node) {
-  return (
-    j.MemberExpression.check(node.left) &&
-    j.ThisExpression.check(node.left.object) &&
-    j.Identifier.check(node.left.property) &&
-    node.left.property.name === "sandbox"
-  );
-}
-
-function isSandboxCreation(j, node) {
-  return (
-    j.CallExpression.check(node.right) &&
-    j.MemberExpression.check(node.right.callee) &&
-    j.MemberExpression.check(node.right.callee.object) &&
-    node.right.callee.object.object.name === "sinon" &&
-    node.right.callee.object.property.name === "sandbox" &&
-    node.right.callee.property.name === "create"
-  );
-}
-
-function isSandboxCreationExpression(j, node) {
-  return isSandboxAssignment(j, node) && isSandboxCreation(j, node);
-}
-
 // Press ctrl+space for code completion
 module.exports = function(file, api) {
   const j = api.jscodeshift;
 
+  function isSandboxAssignment(node) {
+    return (
+      j.MemberExpression.check(node.left) &&
+      j.ThisExpression.check(node.left.object) &&
+      j.Identifier.check(node.left.property) &&
+      node.left.property.name === "sandbox"
+    );
+  }
+
+  function isSandboxCreation(node) {
+    return (
+      j.CallExpression.check(node.right) &&
+      j.MemberExpression.check(node.right.callee) &&
+      j.MemberExpression.check(node.right.callee.object) &&
+      node.right.callee.object.object.name === "sinon" &&
+      node.right.callee.object.property.name === "sandbox" &&
+      node.right.callee.property.name === "create"
+    );
+  }
+
+  function isSandboxCreationExpression(node) {
+    return isSandboxAssignment(node) && isSandboxCreation(node);
+  }
+
   return j(file.source)
     .find(j.AssignmentExpression)
     .filter(({ node }) => {
-      return isSandboxCreationExpression(j, node);
+      return isSandboxCreationExpression(node);
     })
     .remove()
     .toSource();
