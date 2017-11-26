c68f47877bbb4185d870442494e97a91ea341a95
Steve Calvert <scalvert@linkedin.com>
2017-11-26 15:36:58 -0800

    Adding first test

---
+++ __testfixtures__/ember-sinon-sandbox-codemod/sinon.sandbox.create.output.js
@@ -0,0 +1,11 @@
+import { moduleForAcceptance, test } from "ember-qunit";
+
+moduleForAcceptance("foo", "Acceptance | Foo", {
+  beforeEach() {},
+
+  afterEach() {}
+});
+
+test("it happens", function(assert) {
+  assert.ok(true);
+});
